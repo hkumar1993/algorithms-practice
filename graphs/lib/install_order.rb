4a0348423795040fc84eeb1abbaba1bbaa079d1e
Harsh <harsh@hkumar.me>
2017-10-23 20:32:46 -0700

    Graphs Project

--- graphs/lib/install_order.rb
+++ graphs/lib/install_order.rb
@@ -6,9 +6,19 @@
 # N.B. this is how `npm` works.
 
 # Import any files you need to
-
+require_relative 'graph'
+require_relative 'topological_sort'
 
 
 def install_order(arr)
-
+  vertices = Hash.new()
+  (1..arr.flatten.uniq.max).each do |val|
+    vertices[val] = Vertex.new(val)
+  end
+  arr.each do |packages|
+    Edge.new(vertices[packages[1]], vertices[packages[0]])
+  end
+  res = topological_sort(vertices.values)
+  # byebug
+  res.map { |v| v.value  }
 end
--- graphs/lib/topological_sort.rb
+++ graphs/lib/topological_sort.rb
@@ -1,7 +1,54 @@
 require_relative 'graph'
+require 'byebug'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  sorted = []
+  seen = []
+  top = Queue.new
+  vertices.each do |vertex|
+    top.enqueue(vertex) if vertex.in_edges.empty?
+  end
+  vertices_copy = vertices.map {|v| v.value }
+  until top.empty?
+    current = top.dequeue
+    sorted << current
+    vertices_copy.delete(current.value)
+    current.out_edges.each do |edge|
+      to_vertex = edge.to_vertex
+      to_vertex.in_edges.delete(edge)
+      if to_vertex.in_edges.empty?
+        top.enqueue(to_vertex)
+      end
+    end
+  end
+  return sorted if vertices_copy.empty?
+  []
+end
 
+class Queue
+
+  attr_reader :store
+  include Enumerable
+
+  def each(&block)
+    @store.each(&block)
+  end
+
+  def initialize
+    @store = []
+  end
+
+  def enqueue(value)
+    @store.push(value)
+  end
+
+  def dequeue
+    @store.shift
+  end
+
+  def empty?
+    @store.empty?
+  end
 end
